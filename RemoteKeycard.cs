d495c2805147d05193a242503b967fdfdddbcff9
D4MI4NX <105593627+D4MI4NX@users.noreply.github.com>
2025-07-23 00:52:09 +0200

    Incremented version

--- RemoteKeycard.cs
+++ RemoteKeycard.cs
@@ -10,7 +10,7 @@ namespace RemoteKeycard
         
         public override string Author => "D4MI4NX";
         
-        public override Version Version => new(1, 2, 0);
+        public override Version Version => new(1, 3, 0);
         
         public override Version RequiredExiledVersion => new(9, 6, 0);
 
